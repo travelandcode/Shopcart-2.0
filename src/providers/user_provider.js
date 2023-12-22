5d5db0b8390cc33ea9365a001edc86fbc973f9e0
travelandcode <ryonwalkerjnr2001@gmail.com>
2023-12-22 08:04:02 -0500

    Refactored User Provider

--- src/providers/user_provider.js
+++ src/providers/user_provider.js
@@ -1,9 +1,52 @@
 import React, { createContext, useContext, useReducer } from 'react';
 
 const UserContext = createContext();
+let user =  []
 
 export const ACTIONS = {
     SET_USER: 'set user'
+}
+
+async function fetchUser() {       
+  try {
+    const response = await fetch('http://localhost:3001/auth/user', {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include'
+    });
+    user =  await response.json();
+    return user.user
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}
+
+async function addUser(email,password) {
+  try{
+    const response = await fetch('http://localhost:3001/auth/signup',
+      {
+        method: "POST",
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({email: email, password: password})
+      }
+    )
+
+    if(response.status(201)){
+      return "User has been created"
+    }else{
+      return "User already exists"
+    }
+
+
+  }catch(error){
+    console.log(error)
+  }
 }
 
 const userReducer = (currentUser, action) => {
@@ -22,7 +65,7 @@ export const UserProvider = ({ children }) => {
   const [currentUser, dispatch] = useReducer(userReducer, {user: null});
 
   return (
-    <UserContext.Provider value={{ currentUser, dispatch}}>
+    <UserContext.Provider value={{ currentUser, dispatch, fetchUser}}>
       {children}
     </UserContext.Provider>
   );
