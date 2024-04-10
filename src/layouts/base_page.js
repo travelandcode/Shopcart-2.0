a6af543cb86b0194260e7d5c51af983b4993da78
travelandcode <ryonwalkerjnr2001@gmail.com>
2024-04-10 04:04:09 -0500

    removed dead code

--- src/layouts/base_page.js
+++ src/layouts/base_page.js
@@ -1,5 +1,4 @@
 import NavBar from '../components/navbar';
-import TopNavBar from '../components/top_navbar'
 import MobileTopNavBar from '../components/mobile/mobile_top_navbar';
 import Footer from '../components/footer/footer_section'
 import MobileNavBar from '../components/mobile/mobile_navbar';
