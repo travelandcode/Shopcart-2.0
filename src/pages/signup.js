ac2ebb13196deb44e9c14021551c5bd22a26ed7b
travelandcode <ryonwalkerjnr2001@gmail.com>
2024-04-14 21:57:26 -0500

    created login and signup pages

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -4,17 +4,21 @@ import GoogleLogo from '../assets/google.svg'
 import { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
+import PasswordCriteriaModal from '../components/password_criteria_modal'
 import CreateUserToast from '../components/toasts/createUserToast'
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min'
 import Helper from '../util/helper'
 
-function LoginPage() {
+function SignUpPage({currentView}) {
+    const [ firstName, setFirstName] = useState("")
+    const [ lastName, setLastName] = useState("")
     const [ email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [isValidEmail,setIsValidEmail] = useState(true)
+    const [isFirstNameEmpty,setIsFirstNameEmpty] = useState(false)
+    const [isLastNameEmpty,setIsLastNameEmpty] = useState(false)
     const [isEmailEmpty,setIsEmailEmpty] = useState(false)
     const [isPasswordEmpty,setIsPasswordEmpty] = useState(false)
-    const [isIncorrectCredentials,setIsIncorrectCredentials] = useState(false)
     const [isPasswordHidden, setIsPasswordHidden] = useState(true)
     const [showToast, ] = useState(false)
     const history = useHistory()
@@ -22,6 +26,12 @@ function LoginPage() {
 
     const changePasswordtoHidden = () => {
         setIsPasswordHidden(!isPasswordHidden)
+    }
+    const handleFirstNameChange = (e) => {
+        setFirstName(e.target.value)
+    }
+    const handleLastNameChange = (e) => {
+        setLastName(e.target.value)
     }
     const handleEmailChange = (e) => {
         setEmail(e.target.value)
@@ -36,20 +46,26 @@ function LoginPage() {
     const handleBtn = async () => {
 
         setIsValidEmail(validateEmail(email)) 
+        setIsFirstNameEmpty(helper.isStringEmptyOrNullOrWhitespace(firstName))
+        setIsLastNameEmpty(helper.isStringEmptyOrNullOrWhitespace(lastName))
         setIsEmailEmpty(helper.isStringEmptyOrNullOrWhitespace(email))
         setIsPasswordEmpty(helper.isStringEmptyOrNullOrWhitespace(password))
 
         try{
-            if(isEmailEmpty || isPasswordEmpty) return
-            const response =  await fetch('http://localhost:3001/auth/login', {
+            if(isEmailEmpty || isFirstNameEmpty || isLastNameEmpty || isPasswordEmpty) return 
+            const name = firstName+' '+lastName
+            const response = await fetch('http://localhost:3001/auth/signup', {
                 method: 'POST',
                 headers: {'Content-Type':'application/json'},
-                body: JSON.stringify({email,password}),
+                body: JSON.stringify({name,email,password}),
                 credentials: 'include'
             })
-            const data = await response.json();
-            if(data.message === 'User Login was successful' || response.status === 200) window.location.href = '/home'
-            if(data.message === 'Login Attempt Failed' || response.status === 401) setIsIncorrectCredentials(true)
+
+            if(response.message === 'User was created' || response.status === 201){
+                alert('User was successfully created')
+                history.push('/login')
+            }
+
         }catch(error){
             console.log(error)
         }
@@ -62,6 +78,14 @@ function LoginPage() {
 
             window.location.href = 'http://localhost:3001/auth/google'
 
+            // const response = await fetch('http://localhost:3001/auth/google', {
+            //     method: 'GET',
+            //     headers: {'Content-Type':'application/json'},
+            // })
+            
+            // const data = await response.json();
+            
+            // if(data.message === 'User Login was successful' || response.status === 200) history.push('/')
             
         }catch(error){
             console.error(error)
@@ -94,8 +118,8 @@ function LoginPage() {
     }
 
     return(
-        <div className="[ login-page ][ flex flex-row ][ w-full h-screen bg-[#E8E8E8] ]">
-            <div className='[ login-section ][ w-4/5 h-5/6 ][ flex flex-row ][ mx-auto my-auto ][ bg-white ][ rounded-[50px] ][ shadow-2xl ][ p-[40px] ]'>
+        <div className="[ sign-up-page ][ flex flex-row ][ w-full h-screen bg-[#E8E8E8] ]">
+            <div className='[ sign-up-section ][ w-4/5 h-5/6 ][ flex flex-row ][ mx-auto my-auto ][ bg-white ][ rounded-[50px] ][ shadow-2xl ][ p-[40px] ]'>
                 <div className="[ left-panel ][ w-1/2 h-full ][ bg-orange-500 ][ shadow-2xl ][ rounded-[50px] ][ z-[100] ][ px-[40px] ][ relative flex-col ]">
                     <div className='[ left-panel-cotent ]'>
                         <p className='[ mt-[60px] ]mb-[25px] ][ text-[30px] font-bold text-white text-left ]'>Where Every Click Adds Value to Your Life, Filling Your Cart with Joy, Savings, and Everything in Between!</p>      
@@ -106,10 +130,21 @@ function LoginPage() {
                     <div className='[ flex flex-col ][ w-[400px] ][ my-auto mx-auto ]'>
                         <div className='[ login-welcome-section ][ flex flex-col w-full ][ mb-[20px] ]'>
                                 <img className='[ my-auto mx-auto ]' src={Logo} alt='Shopcart Logo'/>
-                                <p className='[ font-sans font-bold text-[30px] ]'>Welcome back to Shopcart</p>
-                                <p className='[ font-sans font-normal text-gray-400 ]'>Login to your account</p>
+                                <p className='[ font-sans font-bold text-[30px] ]'>Welcome to Shopcart</p>
+                                <p className='[ font-sans font-normal text-gray-400 ]'>Register your account</p>
                         </div>
                         <div className='[ user-input ][ flex flex-col justify-end ]'>
+                            <div className='[ flex flex-row mb-[20px] justify-between ]'>
+                                <div className="[ first-name-container ][ h-auto w-[49%] ][ rounded-[15px] ][ flex flex-col ][ relative ]">
+                                    <input onChange={handleFirstNameChange} value={firstName} className="[ first-name-input ][ h-[60px] w-full ][ bg-[#E8E8E8] ][ outline-none ][ focus:border-none ][ my-auto ][ rounded-[15px] ][ pl-[20px] pr-[50px] ][ placeholder:text-[#808080] text-[#808080] ]" placeholder="First Name" required/>
+                                    {isFirstNameEmpty && <p className='[ first-name-error-message ][ text-left font-sans text-[14px] font-bold ][ mt-[3px] text-[#FF3131] ]'>First Name is required</p>}
+                                </div>
+                                
+                                <div className="[ last-name-container ][ h-auto w-[49%] ][ rounded-[15px] ][ flex flex-col ][ relative ]">
+                                    <input onChange={handleLastNameChange} value={lastName} className="[ last-name-input ][ h-[60px] w-full ][ bg-[#E8E8E8] ][ outline-none ][ my-auto ][ rounded-[15px] ][ pl-[20px] pr-[50px] ][ placeholder:text-[#808080] text-[#808080] ]" placeholder="Last Name" required/>
+                                    {isLastNameEmpty && <p className='[ last-name-error-message ][ text-left font-sans text-[14px] font-bold ][ mt-[3px] text-[#FF3131] ]'>Last Name is required</p>}
+                                </div>
+                            </div>
                             <div className="[ email-container ][ w-full h-[60px] ][ rounded-[15px] ][ flex ][ relative ]">
                                 <input onChange={handleEmailChange} value={email} className="[ email-input ][ h-full w-full ][ bg-[#E8E8E8] ][ outline-none ][ my-auto ][ rounded-[15px] ][ pl-[20px] pr-[50px] ][ placeholder:text-[#808080] text-[#808080] ]" placeholder="Email address" required/>
                             </div>
@@ -127,17 +162,14 @@ function LoginPage() {
                                 </button>
                             </div>
                             {isPasswordEmpty &&<p className='[ password-error-message ][ text-left font-sans text-[14px] font-bold ][ mt-[3px] text-[#FF3131] ]'>Password is required</p>}
-                            {isIncorrectCredentials &&<p className='[ incorrect-credentials-error-message ][ text-left font-sans text-[14px] font-bold ][ mt-[3px] text-[#FF3131] ]'>Incorrect Email or Password</p>}
-                            <button className='[ flex justify-end ][ mt-[10px] ][ hover:text-gray-300 ]'>
-                                <p className='text-gray-400 hover:text-gray-300'>Forgot Password?</p>
-                            </button>
+                            { password.length > 0 && PasswordCriteriaModal(password)}
                         </div>
                         <button type='submit' onClick={() =>{handleBtn()}} className='[ login-btn ][ bg-orange-400 ][ w-full h-[50px] ][ rounded-[15px] ][ text-white font-medium text-[16px] ][ mt-[25px] ][ hover:bg-orange-300 ]'>
-                            Login
+                            Sign Up
                         </button>   
                         <div class="relative flex py-5 items-center w-5/6 mx-auto">
                             <div class="flex-grow border-t border-gray-400"></div>
-                            <span class="flex-shrink mx-4 text-gray-400">Or Login With</span>
+                            <span class="flex-shrink mx-4 text-gray-400">Or Register With</span>
                             <div class="flex-grow border-t border-gray-400"></div>
                         </div>
                         <button onClick={() => {handleGoogleBtn()}} class="[ google-btn ][ flex ][ border-2 border-solid border-[#E8E8E8] ][ h-[60px] w-full ][ rounded-[15px] ][ hover:bg-gray-200 ]">
@@ -147,8 +179,8 @@ function LoginPage() {
                             </div>
                         </button>
                         <div className='[ mt-[25px] ][ mx-auto flex flex-row ]'>
-                            <p className='[ font-[Roboto] text-[16px] font-medium ]'>Don't have an account?</p>
-                            <a href='/signup' className='[ ml-[5px] ][ text-orange-400 text-[16px] font-bold font-[Roboto] ][ hover:text-orange-300'>Sign Up</a>
+                            <p className='[ font-[Roboto] text-[16px] font-medium ]'>Already have an account?</p>
+                            <a href='/login' className='[ ml-[5px] ][ text-orange-400 text-[16px] font-bold font-[Roboto] ][ hover:text-orange-300'>Login</a>
                         </div>
                     </div>
                 </div>
@@ -161,4 +193,4 @@ function LoginPage() {
 }
 
 
-export default LoginPage;
+export default SignUpPage
