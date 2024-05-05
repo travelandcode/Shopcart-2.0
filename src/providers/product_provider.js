2f5b8014fb0afd6b3d46ff59f18065516fabda96
travelandcode <ryonwalkerjnr2001@gmail.com>
2024-05-05 07:17:23 -0500

    refactored product provider

--- src/providers/product_provider.js
+++ src/providers/product_provider.js
@@ -1,35 +1,41 @@
 import React, { createContext, useContext, useEffect, useState} from 'react';
 // Create a context
-const ProductContext = createContext({});
+export const ProductContext = createContext({
+    storeProducts: [],
+    fetchProducts: () => {}
+})
 
 export function useProduct() {
     return useContext(ProductContext)
 }
 
-export function ProductProvider({children}) {
-    const [storeProducts, setProducts] = useState(null)
 
-    // useEffect(() =>{
-    //     localStorage.setItem("products",JSON.stringify(storeProducts));
-    // },[storeProducts])
+export const ProductProvider = ({children}) => {
+    const [storeProducts, setProducts] = useState([])
 
-    async function fetchProducts(){
+      const fetchProducts = async() => {
         try {
             const response = await fetch(`${process.env.REACT_APP_API_URL}/products/`)
             if (!response.ok) {
                 throw new Error(`Failed to fetch products: ${response.status} - ${response.statusText}`);
             }
-            
             const result = await response.json()
             setProducts(result.data)
+            sessionStorage.setItem("products",JSON.stringify(result.data))
         } catch (error) {
             console.error('Error fetching products:', error.message)
         }
     }
 
     useEffect(() => {
-        fetchProducts()
-    }, []);
-    
-    return <ProductContext.Provider value={{ storeProducts }}>{children}</ProductContext.Provider>
+        const products = JSON.parse(sessionStorage.getItem("products"))
+        if(products){
+            setProducts(products)
+            console.log("HIT")
+        }
+        else{
+            fetchProducts()
+        }
+    }, [])
+    return <ProductContext.Provider value={{ storeProducts, fetchProducts }}>{children}</ProductContext.Provider>
 }
