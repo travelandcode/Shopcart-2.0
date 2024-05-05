08632b02fed3e5dbe76966685cf9c247838d4263
travelandcode <ryonwalkerjnr2001@gmail.com>
2024-05-05 07:12:47 -0500

    added motion animations to buttons

--- src/components/cart/cart_total.js
+++ src/components/cart/cart_total.js
@@ -5,6 +5,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useCart } from '../../providers/cart_provider';
 import { useProduct } from '../../providers/product_provider';
 import { loadStripe } from '@stripe/stripe-js';
+import {motion} from 'framer-motion'
 
 
 function CartTotal(){
@@ -17,7 +18,6 @@ function CartTotal(){
     };
 
     const { cartProducts } = useCart()
-    console.log(cartProducts)
     const { storeProducts } = useProduct()
     const handleDeliveryCost = (value) => {
         value ? setDeliveryCost(9.99) : setDeliveryCost(0.00);
@@ -77,59 +77,60 @@ function CartTotal(){
     }
 
     return (
-        <div className="[ cart-total ][ flex flex-col ][ px-[20px] py-[20px] ][ ml-[50px] ][ rounded-[15px] ][ z-[1] ][ w-[300px] h-[575px] ][ border-2 border-solid border-[#D3D3D3] ][ bg-white ]">
-            <p className={`[ delivery-txt ][ text-[20px] text-left ][ mb-[10px] ][ font-[Pacifico] font-normal text-[#404040] ] ${ cartProducts.length === 0 ? 'mb-[10px]': ''}`}>Delivery</p>
+        cartProducts.length !== 0 &&
+        <div className="[ cart-total ][ flex flex-col ][ px-[20px] py-[20px] ][ ml-[50px] ][ rounded-[15px] ][ z-[1] ][ w-[400px] h-[700px] ]">
+            <p className={`[ delivery-txt ][ text-[28px] text-left ][ mb-[10px] ][ font-[Pacifico] font-normal text-[#404040] ] ${ cartProducts.length === 0 ? 'mb-[10px]': ''}`}>Delivery</p>
             <DeliverySwitch onExpressChange={handleExpressChange} />
             {
                 cartProducts.length > 0 &&
-                <div className='[ delivery-date-section ][ flex flex-row ][ mb-[30px] ]'>
-                    <p className='[ delivery-date-label ][ font-[Roboto] ][ mr-[5px] ][ text-[14px] text-[#888888] ]'>Delivery date:</p>
-                    <p className='[ delivery-date-txt ][ font-[Roboto] ][ text-[14px] text-[#888888] ]'>{formatDate()}</p>
+                <div className='[ delivery-date-section ][ flex flex-row ][ mb-[20px] ]'>
+                    <p className='[ delivery-date-label ][ font-[Roboto] ][ mr-[5px] ][ text-[16px] text-[#888888] ]'>Delivery date:</p>
+                    <p className='[ delivery-date-txt ][ font-[Roboto] ][ text-[16px] text-[#888888] ]'>{formatDate()}</p>
                 </div>
             }
-            <div className={`[ promo-area ][ h-[120px] w-full ][ border-y-2 border-[#BEBEBE] border-dashed ][ py-[20px] ] ${ cartProducts.length === 0 ? 'mt-[10px]': 'mt-[0px]'}`}>
-                <div className='[ promo-code-section ][ w-full h-[40px] ][ border-[2px] border-solid border-[#B8B8B8] ][ rounded-[10px] ][ mb-[5px] ][ flex flex-row ]'>
+            <div className={`[ promo-area ][ h-[120px] w-full ][ border-y-2 border-[#BEBEBE] border-dashed ][ py-[15px] ] ${ cartProducts.length === 0 ? 'mt-[10px]': 'mt-[0px]'}`}>
+                <div className='[ promo-code-section ][ w-full h-[50px] ][ border-[2px] border-solid border-[#B8B8B8] ][ rounded-[10px] ][ mb-[5px] ][ flex flex-row ]'>
                     <input className="[ promo-code-input ][ rounded-[10px] ][ w-[75%] ][ px-2 ][ outline-none ][ font-[Lobster] ]" type='text' placeholder="PromoCode" />
                     <div className='[ apply-btn ][ h-full [ w-[25%] ][ rounded-[10px] ][ border-l-[2px] border-solid border-[#B8B8B8] ][ hover:bg-[#DCDCDC] ]'>
-                        <p className='[ apply-txt ][ mt-[4px] ][ text-center font-[Lobster] text-[18px] text-[#484848] font-semibold ][ cursor-pointer ]'>Apply</p>
+                        <p className='[ apply-txt ][ mt-[5px] ][ text-center font-[Lobster] text-[18px] text-[#484848] font-semibold ][ cursor-pointer ]'>Apply</p>
                     </div>
                 </div> 
                 <p className='[ discount-txt ][ text-left ][ font-[Roboto] ][ text-[14px] text-[#888888] ]'>20% off Discount</p> 
             </div>
-            <div className='[ subtotal-section ][ py-[20px] ][ border-b-2 border-[#BEBEBE] border-dashed ]'>
+            <div className='[ subtotal-section ][ py-[15px] ][ border-b-2 border-[#BEBEBE] border-dashed ]'>
                 <div className='[ subtotal-line ][ flex flex-row ][ justify-between ]'>
-                    <p className='[ subtotal-label ][ text-[18px] font-[Roboto] text-[#404040] ]'>Subtotal</p>
-                    <p className='[ subtotal-txt ][ text-[18px] font-[Pacifico] text-[#404040] ]'>${parseFloat(subTotal).toFixed(2)}</p>
+                    <p className='[ subtotal-label ][ text-[20px] font-[Roboto] text-[#404040] ]'>Subtotal</p>
+                    <p className='[ subtotal-txt ][ text-[20px] font text-[#404040] font-semibold ]'>${parseFloat(subTotal).toFixed(2)}</p>
                 </div>
                 <div className='[ discount-line ][ mt-2 ][ flex flex-row ][ justify-between ]'>
-                    <p className='[ discount-label ][ text-[#888888] text-[14px] font-[Roboto] ]'>Discount</p>
-                    <p className='[ discount-txt ][ text-[#888888] text-[14px] font-[Pacifico] ]'>${parseFloat(discountCost).toFixed(2)}</p>
+                    <p className='[ discount-label ][ text-[#888888] text-[16px] font-[Roboto] ]'>Discount</p>
+                    <p className='[ discount-txt ][ text-[#888888] text-[16px] font-medium ]'>${parseFloat(discountCost).toFixed(2)}</p>
                 </div>
                 <div className='[ delivery-line ][ mt-2 ][ flex flex-row ][ justify-between ]'>
                     <div className='[ delivery-label-section ][ flex flex-row ]'>
-                        <p className='[ delivery-label ][ text-[#888888] text-[14px] font-[Roboto] ]'>Delivery</p>
+                        <p className='[ delivery-label ][ text-[#888888] text-[16px] font-[Roboto] ]'>Delivery</p>
                         <FontAwesomeIcon className='[ my-auto ml-1 ]' icon={faCircleInfo} style={{color: "#888888",}} />
                     </div>
-                    <p className='[ delivery-txt ][ text-[#888888] text-[14px] font-[Pacifico] ]'>{`$${deliveryCost}`}</p>
+                    <p className='[ delivery-txt ][ text-[#888888] text-[16px] font-medium ]'>{`$${deliveryCost}`}</p>
                 </div>
                 <div className='[ tax-line ][ mt-2 ][ flex flex-row ][ justify-between ]'>
                 <div className='[ tax-label-section ][ flex flex-row ]'>
-                        <p className='[ tax-label ][ text-[#888888] text-[14px] font-[Roboto] ]'>Tax</p>
+                        <p className='[ tax-label ][ text-[#888888] text-[16px] font-[Roboto] ]'>Tax</p>
                         <FontAwesomeIcon className='[ my-auto ml-1 ]' icon={faCircleInfo} style={{color: "#888888",}} />
                     </div>
-                    <p className='[ tax-txt ][ text-[#888888] text-[14px] font-[Pacifico] ]'>${parseFloat(tax).toFixed(2)}</p>
+                    <p className='[ tax-txt ][ text-[#888888] text-[16px] font-medium ]'>${parseFloat(tax).toFixed(2)}</p>
                 </div>
             </div>
             <div className='[ total-section ][ mt-[15px] ][ flex flex-row ][ justify-between ]'>
                 <p className='[ total-label ][ my-auto ][ text-[20px] font-[Roboto] text-[#404040] font-bold ]'>Total</p>
-                <p className='[ total-txt ][ my-auto ][ text-[20px] font-[Pacifico] text-[#404040] font-bold ]'>${parseFloat(total).toFixed(2)}</p>
+                <p className='[ total-txt ][ my-auto ][ text-[20px] font-[] text-[#404040] font-bold ]'>${parseFloat(total).toFixed(2)}</p>
             </div>
-            <button onClick={() => handleSubmit()} className='[ proceed-btn ][ w-full h-[35px] ][ bg-[#FFA33C] hover:bg-[#F3B664] ][ rounded-[5px] ][ cursor-pointer ][ mt-[10px] ]'>
-                <p className='[ proceed-txt ][ mt-[3px] ][ text-[18px] text-white font-[Lobster] ]'>Proceed To Checkout</p>
-            </button>
-            <a href='/' className='[ continue-btn ][ w-full h-[35px] ][ mt-[10px] ][ cursor-pointer ][ rounded-[5px] ][ border-2 border-solid border-[#D3D3D3] ][ hover:bg-[#DCDCDC] hover:border-[#DCDCDC] ]'>
-                <p className='[ continue-txt ][ mt-[3px] ][ text-[18px] text-[#808080] font-[Lobster] ]'>Continue Shopping</p>
-            </a>
+            <motion.button whileHover={{ scale: 1.2 }} onHoverStart={e => {}} onHoverEnd={e => {}} onClick={() => handleSubmit()} className='[ proceed-btn ][ w-full h-[50px] ][ bg-[#FFA33C] hover:bg-[#F3B664] ][ rounded-[5px] ][ z-[1] ][ shadow-lg ][ cursor-pointer ][ mt-[10px] ][ flex ]'>
+                <p className='[ proceed-txt ][ mx-auto my-auto ][ text-[18px] text-white font-sans font-bold ]'>Proceed To Checkout</p>
+            </motion.button>
+            <motion.a href='/' whileHover={{ scale: 1.2 }} onHoverStart={e => {}} onHoverEnd={e => {}} className='[ continue-btn ][ w-full h-[50px] ][ mt-[10px] ][ cursor-pointer ][ rounded-[5px] ][ z-[1] ][ shadow-lg ][ border-2 border-solid border-[#D3D3D3] ][ hover:bg-[#DCDCDC] hover:border-[#DCDCDC] ][ flex ]'>
+                <p className='[ continue-txt ][ my-auto mx-auto ][ text-[18px] text-[#808080] font-sans font-bold ]'>Continue Shopping</p>
+            </motion.a>
         </div>
     );
 }
